ad429bccd56fc70b1fa802ed8defba4d8bd5d1fc
dyrkow <webworkdeveloper@gmail.com>
2024-01-31 23:03:30 +0300

    Initial

---
+++ index.ts
@@ -0,0 +1,15 @@
+module.exports = {
+    meta: {
+        type: 'layout',
+        docs: {
+            description: 'Sort imports by length'
+        },
+        fixable: 'code',
+        schema: [] // no options
+    },
+    create: function (context) {
+        return {
+            // callback functions
+        };
+    }
+};
